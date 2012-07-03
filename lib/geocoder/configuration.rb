68cbb55d779c6f8c30199de81fad2e1fee8e2c67
jessecurry <jesse@jessecurry.net>
2012-07-03 13:51:34 -0400

    Adding files for geoip omni

--- lib/geocoder/configuration.rb
+++ lib/geocoder/configuration.rb
@@ -60,16 +60,17 @@ module Geocoder
     end
 
     def set_defaults
-      @timeout      = 3           # geocoding service timeout (secs)
-      @lookup       = :google     # name of geocoding service (symbol)
-      @language     = :en         # ISO-639 language code
-      @http_headers = {}          # HTTP headers for lookup
-      @use_https    = false       # use HTTPS for lookup requests? (if supported)
-      @http_proxy   = nil         # HTTP proxy server (user:pass@host:port)
-      @https_proxy  = nil         # HTTPS proxy server (user:pass@host:port)
-      @api_key      = nil         # API key for geocoding service
-      @cache        = nil         # cache object (must respond to #[], #[]=, and #keys)
-      @cache_prefix = "geocoder:" # prefix (string) to use for all cache keys
+      @timeout        = 3           # geocoding service timeout (secs)
+      @lookup         = :google     # name of geocoding service (symbol)
+      @language       = :en         # ISO-639 language code
+      @http_headers   = {}          # HTTP headers for lookup
+      @use_https      = false       # use HTTPS for lookup requests? (if supported)
+      @http_proxy     = nil         # HTTP proxy server (user:pass@host:port)
+      @https_proxy    = nil         # HTTPS proxy server (user:pass@host:port)
+      @api_key        = nil         # API key for geocoding service
+      @geoip_api_key  = nil         # API key for ip geocoding service
+      @cache          = nil         # cache object (must respond to #[], #[]=, and #keys)
+      @cache_prefix   = "geocoder:" # prefix (string) to use for all cache keys
 
       # exceptions that should not be rescued by default
       # (if you want to implement custom error handling);
